acae070ddcdf1e6165961ed6ca4f220d46c9c27e
Nicolas Golob <nicolasgolob@gmail.com>
2021-08-29 15:25:06 +0200

    express add + test and react add

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 5000;
 
 const app  = express();
 
+app.use(express.json())
+
+app.get('/api/nicolas', (_, res)=>{
+    res.send({
+        message:'Bonjour tout le monde ! '
+    })
+})
+
 app.listen(5000,()=>{
     console.log('Le serveur 5000 est lancé.');
 });
